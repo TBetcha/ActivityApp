50fd8d249b18f2cead3ffebfa44c0d5e86d7bc47
TMB <Troy.Boettger@gmail.com>
2021-12-11 04:16:22 -0500

    endpoint fix

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
   [ApiController]
-  [Route("[controller]")]
-  public class BaseApiController:ControllerBase
+  [Route("api/[controller]")]
+  public class BaseApiController : ControllerBase
   {
     
   }
