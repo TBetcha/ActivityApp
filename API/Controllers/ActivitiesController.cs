783a2ef1f8d8d8b813aa063fcd8c663cb68846c1
TMB <Troy.Boettger@gmail.com>
2021-12-12 23:26:03 -0500

    start dependency injection and refactor

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -13,23 +15,17 @@ namespace API.Controllers
 {
   public class ActivitiesController : BaseApiController
   {
-    private readonly DataContext _context;
-
-    public ActivitiesController(DataContext context)
-    {
-      _context = context;
-    }
 
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-      return await _context.Activities.ToListAsync();
+      return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-      return await _context.Activities.FindAsync(id);
+      return Ok();
     }
   }
 }
