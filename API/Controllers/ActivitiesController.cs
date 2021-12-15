718366ad467297fcb0b667d76ae651b653d3af0c
TMB <Troy.Boettger@gmail.com>
2021-12-14 21:47:13 -0500

    create activity endpoint

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,6 @@ namespace API.Controllers
 {
   public class ActivitiesController : BaseApiController
   {
-
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
@@ -25,7 +24,13 @@ namespace API.Controllers
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-      return await Mediator.Send(new Details.Query{Id = id});
+      return await Mediator.Send(new Details.Query {Id = id});
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAcitivity(Activity activity)
+    {
+      return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
     }
   }
 }
